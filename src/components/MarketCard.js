fd94966ba8cf7772dbd4979641e5c5a63a8a6490
EBrain <ebrain.ebrain@gmail.com>
2023-03-13 21:14:48 +0630

    Add routing

--- src/components/MarketCard.js
+++ src/components/MarketCard.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
 import styles from './MarketCard.module.css';
 
-function MarketCard({ market, expanded = false, dark = false }) {
+function MarketCard({ market, expanded, dark }) {
   return (
-    <article
-      className={cn(
-        styles.card,
-        expanded && styles.expanded,
-        dark && styles.dark,
-      )}
-    >
-      <div
-        className={styles.imgContainer}
-        style={{ backgroundImage: `url(${market.image})` }}
+    <Link to={market.id}>
+      <article
+        className={cn(
+          styles.card,
+          expanded && styles.expanded,
+          dark && styles.dark,
+        )}
       >
-        {/* TODO: add arrow icon */}
-      </div>
-      <div className={styles.details}>
-        <h2 className={styles.name}>{market.name}</h2>
-        <p className={styles.price}>{market.price}</p>
-      </div>
-    </article>
+        <div
+          className={styles.imgContainer}
+          style={{ backgroundImage: `url(${market.image})` }}
+        >
+          {/* TODO: add arrow icon */}
+        </div>
+        <div className={styles.details}>
+          <h2 className={styles.name}>{market.name}</h2>
+          <p className={styles.price}>{market.price}</p>
+        </div>
+      </article>
+    </Link>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import styles from './Navbar.module.css';
 
 function Navbar() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Crypto Metrics</h1>
+      <h1 className={styles.title}>
+        <NavLink to="/">Crypto Metrics</NavLink>
+      </h1>
     </header>
   );
 }
